b4809480448f0b0e003de991cea9dbc3e72350a5
MayhemMark <mayhem@mark.try>
2019-05-26 04:07:47 +0200

    cleaner version

--- calculation.py
+++ calculation.py
@@ -2,14 +2,10 @@ def get_weight_for_input(options, input):
     pieces = input.split(" ")
 
     for item in options:
-        for abb in item.abbreviations:
-            if pieces[1] == abb:
-                return (
-                    float(pieces[0]),
-                    item
-                )
-    
+        if pieces[1] in item.abbreviations:
+            return (float(pieces[0]), item)
+
     # return een lege lijst als de code hieraan toe komt
     return (
-        pieces[0]
+        float(pieces[0])
     )
--- p0mp.py
+++ p0mp.py
@@ -1,4 +1,5 @@
 from calculation import get_weight_for_input
+import sys
 class WeightUnit:
     def __init__(self, label, unitPerKilo, abbreviations):
         self.label = label
@@ -9,37 +10,36 @@ class WeightUnit:
         return "<%s: %s>" % (self.__class__.__name__, self.label)
 
 list_options = (
-    WeightUnit(
-        "Pounds",
-        (1/0.45),
-        ("p", "pd", "pds", "lbs", "lb")
-    ),
-    WeightUnit(
-        "Stone",
-        25,
-        ("s", "st", "stone")
-    ),
-
     WeightUnit("Kilos", 1, ("kg", "kg.", "kilos", "kilo")),
     WeightUnit("Gram", 1000, ("gr","gram","gr.","grm")),
+
+    WeightUnit("Pounds", (1/0.45), ("p", "pd", "pds", "lbs", "lb")),
+    WeightUnit("Stone", 25, ("s", "st", "stone")),
 
     # Sexagsimal table of units
     WeightUnit("Talent", 1/30.24, ("t", "tlt", "tl", "tlnt")),
     WeightUnit("Mina", 1/0.504, ("m", "mina")),
     WeightUnit("Karsa", 1/0.084, ("k", "karsa")),
+    WeightUnit("zwz", 1/0.0042, ("z", "zwz")),
+    WeightUnit("Danake", 1/0.00139, ("d", "danake")),
+    WeightUnit("Halluru", 1/0.00021, ("h")),
+
+    # Need to keep the "shekel" alias or this entry is not going to work for "sh" for some reason
     WeightUnit("Shekel", 1/0.0084, ("sh")),
-    WeightUnit("zwz", 1/0.0042, ("z")),
-    WeightUnit("Danake", 1/0.00139, ("d")),
-    WeightUnit("Halluru", 1/0.00021, ("x")),
 )
 
-input = get_weight_for_input(list_options, input("Weight:"))
-print(input)
+if (len(sys.argv) >= 3):
+    raw_input_str = sys.argv[1] + ' ' + sys.argv[2]
+elif (len(sys.argv) >= 2):
+    raw_input_str = sys.argv[1]
+else:
+    raw_input_str = input("Weight:")
+
+input = get_weight_for_input(list_options, raw_input_str)
 input_type = input[1]
+input_quantity = input[0]
 
-input_quantity = float(input[0])
 print(f"{input_quantity} {input_type.label} is the same as:")
-
 for item in list_options:
     input_quantity_calculated = (input_quantity/input_type.unitPerKilo) * item.unitPerKilo
     print(f'  {input_quantity_calculated} {item.label}')
